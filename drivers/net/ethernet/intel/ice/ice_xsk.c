3aa7df600a6d77d5be8327355aa72ac3ae336b24
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2024-03-21 14:15:53 +0100

    ice: toggle netif_carrier when setting up XSK pool
    This so we prevent Tx timeout issues. One of conditions checked on
    running in the background dev_watchdog() is netif_carrier_ok(), so let
    us turn it off when we disable the queues that belong to a q_vector
    where XSK pool is being configured.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>

--- drivers/net/ethernet/intel/ice/ice_xsk.c
+++ drivers/net/ethernet/intel/ice/ice_xsk.c
@@ -172,6 +172,8 @@ static int ice_qp_dis(struct ice_vsi *vsi, u16 q_idx)
 	q_vector = rx_ring->q_vector;
 
 	synchronize_net();
+	netif_trans_update(vsi->netdev);
+	netif_carrier_off(vsi->netdev);
 	netif_tx_stop_queue(netdev_get_tx_queue(vsi->netdev, q_idx));
 
 	ice_qvec_dis_irq(vsi, rx_ring, q_vector);
@@ -235,6 +237,7 @@ static int ice_qp_ena(struct ice_vsi *vsi, u16 q_idx)
 	ice_qvec_ena_irq(vsi, q_vector);
 
 	netif_tx_start_queue(netdev_get_tx_queue(vsi->netdev, q_idx));
+	netif_carrier_on(vsi->netdev);
 
 	return fail;
 }
