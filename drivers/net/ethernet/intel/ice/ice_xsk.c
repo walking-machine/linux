7e90e31c7c7fde24bf75b59a2fa7e600fdd05b88
Larysa Zaremba <larysa.zaremba@intel.com>
2024-05-24 13:27:55 +0200

    ice: fix locking in ice_xsk_pool_setup()
    With ICE_CFG_BUSY PF state flag locking used in ice_xdp(), there is no need
    to lock with VSI state in ice_xsk_pool_setup(). Also, same as in
    ice_xdp_setup_prog(), for robust synchronization state between reset
    preparation and PF VSI rebuild has to be handled.
    
    Remove locking logic from ice_qp_dis() and ice_qp_ena() and skip those
    functions, if rebuild is pending.
    
    Fixes: 2d4238f55697 ("ice: Add support for AF_XDP")
    Signed-off-by: Larysa Zaremba <larysa.zaremba@intel.com>

--- drivers/net/ethernet/intel/ice/ice_xsk.c
+++ drivers/net/ethernet/intel/ice/ice_xsk.c
@@ -163,7 +163,6 @@ static int ice_qp_dis(struct ice_vsi *vsi, u16 q_idx)
 	struct ice_tx_ring *xdp_ring;
 	struct ice_tx_ring *tx_ring;
 	struct ice_rx_ring *rx_ring;
-	int timeout = 50;
 	int fail = 0;
 	int err;
 
@@ -174,13 +173,6 @@ static int ice_qp_dis(struct ice_vsi *vsi, u16 q_idx)
 	rx_ring = vsi->rx_rings[q_idx];
 	xdp_ring = vsi->xdp_rings[q_idx];
 	q_vector = rx_ring->q_vector;
-
-	while (test_and_set_bit(ICE_CFG_BUSY, vsi->state)) {
-		timeout--;
-		if (!timeout)
-			return -EBUSY;
-		usleep_range(1000, 2000);
-	}
 
 	synchronize_net();
 	netif_trans_update(vsi->netdev);
@@ -251,7 +243,6 @@ static int ice_qp_ena(struct ice_vsi *vsi, u16 q_idx)
 	synchronize_net();
 	netif_tx_start_queue(netdev_get_tx_queue(vsi->netdev, q_idx));
 	netif_carrier_on(vsi->netdev);
-	clear_bit(ICE_CFG_BUSY, vsi->state);
 
 	return fail;
 }
@@ -379,7 +370,8 @@ int ice_xsk_pool_setup(struct ice_vsi *vsi, struct xsk_buff_pool *pool, u16 qid)
 		return -EINVAL;
 	}
 
-	if_running = netif_running(vsi->netdev) && ice_is_xdp_ena_vsi(vsi);
+	if_running = !ice_rebuild_pending(vsi) &&
+		     netif_running(vsi->netdev) && ice_is_xdp_ena_vsi(vsi);
 
 	if (if_running) {
 		struct ice_rx_ring *rx_ring = vsi->rx_rings[qid];
