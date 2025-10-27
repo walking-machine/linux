48ae72e2cb89059117d0f9520925d4ee62e83e2e
Larysa Zaremba <larysa.zaremba@intel.com>
2025-10-27 13:32:49 +0100

    ixgbevf: implement AF_XDP ZC initialization
    Implement xsk_buff_pool configuration and supporting functionality, such as
    a single queue pair reconfiguration. Also, properly initialize Rx buffers.
    
    Signed-off-by: Larysa Zaremba <larysa.zaremba@intel.com>

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -30,10 +30,11 @@
 #include <linux/bpf.h>
 #include <linux/bpf_trace.h>
 #include <linux/atomic.h>
-#include <net/libeth/xdp.h>
+#include <net/libeth/xsk.h>
 #include <net/xfrm.h>
 
 #include "ixgbevf_txrx_lib.h"
+#include "ixgbevf_xsk.h"
 
 const char ixgbevf_driver_name[] = "ixgbevf";
 static const char ixgbevf_driver_string[] =
@@ -523,7 +524,7 @@ void ixgbevf_clean_xdp_num(struct ixgbevf_ring *xdp_ring, bool in_napi,
 	xdp_flush_frame_bulk(&cbulk);
 }
 
-static void ixgbevf_clean_xdp_ring(struct ixgbevf_ring *xdp_ring)
+void ixgbevf_clean_xdp_ring(struct ixgbevf_ring *xdp_ring)
 {
 	ixgbevf_clean_xdp_num(xdp_ring, false, xdp_ring->pending);
 	libeth_xdpsq_put(&xdp_ring->xdpq_lock, xdp_ring->netdev);
@@ -1144,7 +1145,7 @@ static inline void ixgbevf_irq_disable(struct ixgbevf_adapter *adapter)
  * ixgbevf_irq_enable - Enable default interrupt generation settings
  * @adapter: board private structure
  **/
-static inline void ixgbevf_irq_enable(struct ixgbevf_adapter *adapter)
+void ixgbevf_irq_enable(struct ixgbevf_adapter *adapter)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 
@@ -1154,14 +1155,32 @@ static inline void ixgbevf_irq_enable(struct ixgbevf_adapter *adapter)
 }
 
 /**
+ * ixgbevf_xsk_pool_from_q - get ZC XSK buffer pool bound to a queue ID
+ * @ring: Rx or Tx ring
+ *
+ * Return: A pointer to xsk_buff_pool structure if there is a buffer pool
+ * attached, configured as zero-copy, and usable by this queue, NULL otherwise.
+ */
+static struct xsk_buff_pool *ixgbevf_xsk_pool_from_q(struct ixgbevf_ring *ring)
+{
+	struct xsk_buff_pool *pool =
+		xsk_get_pool_from_qid(ring->netdev, ring->queue_index);
+
+	if (!READ_ONCE(ring->xdp_prog) && !ring_is_xdp(ring))
+		return NULL;
+
+	return (pool && pool->dev) ? pool : NULL;
+}
+
+/**
  * ixgbevf_configure_tx_ring - Configure 82599 VF Tx ring after Reset
  * @adapter: board private structure
  * @ring: structure containing ring specific data
  *
  * Configure the Tx descriptor ring after a reset.
  **/
-static void ixgbevf_configure_tx_ring(struct ixgbevf_adapter *adapter,
-				      struct ixgbevf_ring *ring)
+void ixgbevf_configure_tx_ring(struct ixgbevf_adapter *adapter,
+			       struct ixgbevf_ring *ring)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 	u64 tdba = ring->dma;
@@ -1218,6 +1237,12 @@ static void ixgbevf_configure_tx_ring(struct ixgbevf_adapter *adapter,
 		libeth_xdpsq_get(&ring->xdpq_lock, ring->netdev,
 				 num_possible_cpus() > adapter->num_xdp_queues);
 	}
+
+	ring->xsk_pool = ixgbevf_xsk_pool_from_q(ring);
+	if (ring_is_xdp(ring) && ring->xsk_pool)
+		set_ring_xsk(ring);
+	else
+		clear_ring_xsk(ring);
 
 	clear_bit(__IXGBEVF_HANG_CHECK_ARMED, &ring->state);
 	clear_bit(__IXGBEVF_TX_XDP_RING_PRIMED, &ring->state);
@@ -1288,8 +1313,8 @@ static void ixgbevf_setup_psrtype(struct ixgbevf_adapter *adapter)
 }
 
 #define IXGBEVF_MAX_RX_DESC_POLL 10
-static void ixgbevf_disable_rx_queue(struct ixgbevf_adapter *adapter,
-				     struct ixgbevf_ring *ring)
+void ixgbevf_disable_rx_queue(struct ixgbevf_adapter *adapter,
+			      struct ixgbevf_ring *ring)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 	int wait_loop = IXGBEVF_MAX_RX_DESC_POLL;
@@ -1315,8 +1340,8 @@ static void ixgbevf_disable_rx_queue(struct ixgbevf_adapter *adapter,
 		       reg_idx);
 }
 
-static void ixgbevf_rx_desc_queue_enable(struct ixgbevf_adapter *adapter,
-					 struct ixgbevf_ring *ring)
+void ixgbevf_rx_desc_queue_enable(struct ixgbevf_adapter *adapter,
+				  struct ixgbevf_ring *ring)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 	int wait_loop = IXGBEVF_MAX_RX_DESC_POLL;
@@ -1392,18 +1417,33 @@ static void ixgbevf_setup_vfmrqc(struct ixgbevf_adapter *adapter)
 	IXGBE_WRITE_REG(hw, IXGBE_VFMRQC, vfmrqc);
 }
 
-static void ixgbevf_rx_destroy_pp(struct ixgbevf_ring *rx_ring)
+void ixgbevf_rx_destroy_pp(struct ixgbevf_ring *rx_ring)
 {
 	struct libeth_fq fq = {
 		.pp	= rx_ring->pp,
 		.fqes	= rx_ring->rx_fqes,
 	};
 
-	if (!fq.pp)
+	if (!fq.pp && !rx_ring->xsk_fqes)
 		return;
 
 	xdp_rxq_info_detach_mem_model(&rx_ring->xdp_rxq);
 	xdp_rxq_info_unreg(&rx_ring->xdp_rxq);
+
+	if (test_and_clear_bit(__IXGBEVF_RXTX_XSK_RING, &rx_ring->state)) {
+		struct libeth_xskfq xskfq = {
+			.fqes = rx_ring->xsk_fqes,
+		};
+
+		libeth_xskfq_destroy(&xskfq);
+		rx_ring->xsk_fqes = NULL;
+		rx_ring->pending = xskfq.pending;
+		rx_ring->thresh = xskfq.thresh;
+		rx_ring->rx_buf_len = xskfq.buf_len;
+		rx_ring->xsk_pool = NULL;
+
+		return;
+	}
 
 	libeth_rx_fq_destroy(&fq);
 	rx_ring->rx_fqes = NULL;
@@ -1435,8 +1475,43 @@ static int ixgbevf_rx_create_pp(struct ixgbevf_ring *rx_ring)
 						      LIBETH_XDP_HEADROOM :
 						      LIBETH_SKB_HEADROOM),
 	};
+	struct xsk_buff_pool *pool;
 	u32 frame_size;
 	int ret;
+
+	pool = ixgbevf_xsk_pool_from_q(rx_ring);
+	if (pool) {
+		u32 frag_sz = xsk_pool_get_chunk_size(pool);
+		struct libeth_xskfq xskfq = {
+			.nid = numa_node_id(),
+			.count = rx_ring->count,
+			.pool = pool,
+		};
+
+		ret = libeth_xskfq_create(&xskfq);
+		if (ret)
+			return ret;
+
+		rx_ring->xsk_pool = xskfq.pool;
+		rx_ring->xsk_fqes = xskfq.fqes;
+		rx_ring->pending = xskfq.count - 1;
+		rx_ring->thresh = xskfq.thresh;
+		rx_ring->rx_buf_len = xskfq.buf_len;
+		set_ring_xsk(rx_ring);
+
+		ret = __xdp_rxq_info_reg(&rx_ring->xdp_rxq, rx_ring->netdev,
+					 rx_ring->queue_index, 0, frag_sz);
+		if (ret)
+			goto err;
+
+		ret = xdp_rxq_info_reg_mem_model(&rx_ring->xdp_rxq,
+						 MEM_TYPE_XSK_BUFF_POOL,
+						 rx_ring->xsk_pool);
+		if (ret)
+			goto err;
+
+		return 0;
+	}
 
 	/* Some HW requires DMA write sizes to be aligned to 1K,
 	 * which warrants fake header split usage, but this is
@@ -1490,8 +1565,8 @@ err:
 	return ret;
 }
 
-static void ixgbevf_configure_rx_ring(struct ixgbevf_adapter *adapter,
-				      struct ixgbevf_ring *ring)
+void ixgbevf_configure_rx_ring(struct ixgbevf_adapter *adapter,
+			       struct ixgbevf_ring *ring)
 {
 	struct ixgbe_hw *hw = &adapter->hw;
 	union ixgbe_adv_rx_desc *rx_desc;
@@ -1531,6 +1606,7 @@ static void ixgbevf_configure_rx_ring(struct ixgbevf_adapter *adapter,
 	/* reset ntu and ntc to place SW in sync with hardwdare */
 	ring->next_to_clean = 0;
 	ring->next_to_use = 0;
+	ring->pending = ixgbevf_desc_unused(ring);
 
 	ixgbevf_rx_create_pp(ring);
 
@@ -1542,7 +1618,8 @@ static void ixgbevf_configure_rx_ring(struct ixgbevf_adapter *adapter,
 		rxdctl &= ~(IXGBE_RXDCTL_RLPMLMASK | IXGBE_RXDCTL_RLPML_EN);
 		if (pkt_len <= IXGBE_RXDCTL_RLPMLMASK) {
 			rxdctl |= pkt_len | IXGBE_RXDCTL_RLPML_EN;
-			rlpml_valid = true;
+			if (pkt_len <= ring->rx_buf_len)
+				rlpml_valid = true;
 		}
 	}
 
@@ -1552,7 +1629,11 @@ static void ixgbevf_configure_rx_ring(struct ixgbevf_adapter *adapter,
 	IXGBE_WRITE_REG(hw, IXGBE_VFRXDCTL(reg_idx), rxdctl);
 
 	ixgbevf_rx_desc_queue_enable(adapter, ring);
-	ixgbevf_alloc_rx_buffers(ring, ixgbevf_desc_unused(ring));
+
+	if (ring_is_xsk(ring))
+		ixgbevf_xsk_alloc_rx_bufs(ring, ring->pending);
+	else
+		ixgbevf_alloc_rx_buffers(ring, ring->pending);
 }
 
 /**
@@ -1941,8 +2022,13 @@ void ixgbevf_up(struct ixgbevf_adapter *adapter)
  * ixgbevf_clean_rx_ring - Free Rx Buffers per Queue
  * @rx_ring: ring to free buffers from
  **/
-static void ixgbevf_clean_rx_ring(struct ixgbevf_ring *rx_ring)
+void ixgbevf_clean_rx_ring(struct ixgbevf_ring *rx_ring)
 {
+	if (ring_is_xsk(rx_ring)) {
+		ixgbevf_rx_xsk_ring_free_buffs(rx_ring);
+		goto reset;
+	}
+
 	/* Free Rx ring sk_buff */
 	libeth_xdp_return_stash(&rx_ring->xdp_stash);
 
@@ -1955,15 +2041,17 @@ static void ixgbevf_clean_rx_ring(struct ixgbevf_ring *rx_ring)
 			i = 0;
 	}
 
+reset:
 	rx_ring->next_to_clean = 0;
 	rx_ring->next_to_use = 0;
+	rx_ring->pending = 0;
 }
 
 /**
  * ixgbevf_clean_tx_ring - Free Tx Buffers
  * @tx_ring: ring to be cleaned
  **/
-static void ixgbevf_clean_tx_ring(struct ixgbevf_ring *tx_ring)
+void ixgbevf_clean_tx_ring(struct ixgbevf_ring *tx_ring)
 {
 	u16 i = tx_ring->next_to_clean;
 	struct ixgbevf_tx_buffer *tx_buffer = &tx_ring->tx_buffer_info[i];
@@ -2046,7 +2134,7 @@ static void ixgbevf_clean_all_tx_rings(struct ixgbevf_adapter *adapter)
 		ixgbevf_clean_xdp_ring(adapter->xdp_ring[i]);
 }
 
-static void ixgbevf_flush_tx_queue(struct ixgbevf_ring *ring)
+void ixgbevf_flush_tx_queue(struct ixgbevf_ring *ring)
 {
 	u8 reg_idx = ring->reg_idx;
 
@@ -4054,6 +4142,9 @@ static int ixgbevf_xdp(struct net_device *dev, struct netdev_bpf *xdp)
 	switch (xdp->command) {
 	case XDP_SETUP_PROG:
 		return ixgbevf_xdp_setup(dev, xdp->prog, xdp->extack);
+	case XDP_SETUP_XSK_POOL:
+		return ixgbevf_setup_xsk_pool(netdev_priv(dev), xdp->xsk.pool,
+					      xdp->xsk.queue_id);
 	default:
 		return -EINVAL;
 	}
