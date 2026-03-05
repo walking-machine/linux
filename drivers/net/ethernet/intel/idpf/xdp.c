8468091cbe9bab37b33a04c73eb8ef3e1a7c7263
Larysa Zaremba <larysa.zaremba@intel.com>
2026-03-05 12:12:48 +0100

    libeth, idpf: use truesize as XDP RxQ info frag_size
    The only user of frag_size field in XDP RxQ info is
    bpf_xdp_frags_increase_tail(). It clearly expects whole buffer size instead
    of DMA write size. Different assumptions in idpf driver configuration lead
    to negative tailroom.
    
    To make it worse, buffer sizes are not actually uniform in idpf when
    splitq is enabled, as there are several buffer queues, so rxq->rx_buf_size
    is meaningless in this case.
    
    Use truesize of the first bufq in AF_XDP ZC, as there is only one. Disable
    growing tail for regular splitq.
    
    Fixes: ac8a861f632e ("idpf: prepare structures to support XDP")
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Signed-off-by: Larysa Zaremba <larysa.zaremba@intel.com>
    Link: https://patch.msgid.link/20260305111253.2317394-8-larysa.zaremba@intel.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/intel/idpf/xdp.c
+++ drivers/net/ethernet/intel/idpf/xdp.c
@@ -46,11 +46,15 @@ static int __idpf_xdp_rxq_info_init(struct idpf_rx_queue *rxq, void *arg)
 {
 	const struct idpf_vport *vport = rxq->q_vector->vport;
 	bool split = idpf_is_queue_model_split(vport->rxq_model);
+	u32 frag_size = 0;
 	int err;
+
+	if (idpf_queue_has(XSK, rxq))
+		frag_size = rxq->bufq_sets[0].bufq.truesize;
 
 	err = __xdp_rxq_info_reg(&rxq->xdp_rxq, vport->netdev, rxq->idx,
 				 rxq->q_vector->napi.napi_id,
-				 rxq->rx_buf_size);
+				 frag_size);
 	if (err)
 		return err;
 
