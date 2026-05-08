d3cc8aae7d0b7d644c4bfbe5b676b57338260fba
Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
2026-05-08 13:08:56 +0200

    ice: add support for unmanaged DPLL on E830 NIC
    Hardware variants of E830 may support an unmanaged DPLL where the
    configuration is hardcoded within the hardware and firmware, meaning
    users cannot modify settings. However, users are able to check the DPLL
    lock status and obtain configuration information through the Linux DPLL
    and devlink health subsystem.
    
    Availability of 'loss of lock' health status code determines if such
    support is available, if true, register single DPLL device with 1 input
    and 1 output and provide hardcoded/read only properties of a pin and
    DPLL device. User is only allowed to check DPLL device status and receive
    notifications on DPLL lock status change.
    
    When present, the DPLL device locks to an external signal provided
    through the PCIe/OCP pin. The expected input signal is 1PPS
    (1 Pulse Per Second) embedded on a 10MHz reference clock.
    The DPLL produces output:
    - for MAC (Media Access Control) & PHY (Physical Layer) clocks,
    - 1PPS for synchronization of onboard PHC (Precision Hardware Clock) timer.
    
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Signed-off-by: Grzegorz Nitka <grzegorz.nitka@intel.com>
    Signed-off-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Tested-by: Sunitha Mekala <sunithax.d.mekala@intel.com> (A Contingent worker at Intel)

--- drivers/net/ethernet/intel/ice/ice_dpll.h
+++ drivers/net/ethernet/intel/ice/ice_dpll.h
@@ -26,6 +26,8 @@
 #define ICE_CGU_R11_SYNCE_S_BYP_CLK	GENMASK(6, 1)
 
 #define ICE_CGU_BYPASS_MUX_OFFSET_E825C	3
+#define ICE_DPLL_UNMANAGED_PIN_NUM	4
+#define ICE_DPLL_IN_ESYNC_ENABLED	ICE_AQC_GET_CGU_IN_CFG_FLG2_ESYNC_EN
 
 /**
  * enum ice_dpll_pin_sw - enumerate ice software pin indices:
@@ -193,6 +195,7 @@ struct ice_dplls {
 	s32 output_phase_adj_max;
 	u32 periodic_counter;
 	bool generic;
+	bool unmanaged;
 	struct work_struct txclk_work;
 	bool txclk_switch_requested;
 	struct rw_semaphore txclk_notify_rwsem;
@@ -201,9 +204,16 @@ struct ice_dplls {
 #if IS_ENABLED(CONFIG_PTP_1588_CLOCK)
 void ice_dpll_init(struct ice_pf *pf);
 void ice_dpll_deinit(struct ice_pf *pf);
+void ice_dpll_lock_state_set_unmanaged(struct ice_pf *pf,
+				       const struct ice_aqc_health_status_elem *buff,
+				       bool notify);
 #else
 static inline void ice_dpll_init(struct ice_pf *pf) { }
 static inline void ice_dpll_deinit(struct ice_pf *pf) { }
+static inline void
+ice_dpll_lock_state_set_unmanaged(struct ice_pf *pf,
+				  const struct ice_aqc_health_status_elem *buff,
+				  bool notify) { }
 #endif
 
 #endif
