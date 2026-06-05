80efe9e8e3cf6a5e523b3b02bbd7d167d05ae754
Sergey Temerkhanov <sergey.temerkhanov@intel.com>
2026-06-05 13:21:27 -0700

    ice: Zero out the PTP control PF pointer at ice_adapter cleanup
    Zero out the ctrl_pf pointer in ice_adapter when the control PF is removed.
    This prevents potential dangling pointer dereference when accessing
    PTP-related structures from other PFs of the same adapter.
    
    Fixes: e800654e85b5b ("ice: Use ice_adapter for PTP shared data instead of auxdev")
    Signed-off-by: Sergey Temerkhanov <sergey.temerkhanov@intel.com>
    Reported-by: Frederick Lawler <fred@cloudflare.com>
    Closes: https://lkml.indiana.edu/2507.3/01388.html
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Tested-by: Frederick Lawler <fred@cloudflare.com>

--- drivers/net/ethernet/intel/ice/ice_ptp.c
+++ drivers/net/ethernet/intel/ice/ice_ptp.c
@@ -3095,6 +3095,18 @@ static void ice_ptp_setup_adapter(struct ice_pf *pf)
 	rcu_assign_pointer(pf->adapter->ctrl_pf, pf);
 }
 
+static void ice_ptp_cleanup_adapter(struct ice_pf *pf)
+{
+	/* Zero out adapter->ctrl_pf pointer when the ctrl_pf itself
+	 * is being removed to prevent any secondary PFs from accessing
+	 * it after it is deleted.
+	 */
+	if (cmpxchg(&pf->adapter->ctrl_pf,
+		    (struct ice_pf __rcu *)pf, NULL) ==
+			(struct ice_pf __rcu *)pf)
+		synchronize_rcu();
+}
+
 static int ice_ptp_setup_pf(struct ice_pf *pf)
 {
 	struct ice_ptp *ptp = &pf->ptp;
@@ -3404,6 +3416,8 @@ void ice_ptp_init(struct ice_pf *pf)
 err_clean_pf:
 	mutex_destroy(&ptp->port.ps_lock);
 	ice_ptp_cleanup_pf(pf);
+
+	ice_ptp_cleanup_adapter(pf);
 err_exit:
 	/* If we registered a PTP clock, release it */
 	if (pf->ptp.clock) {
@@ -3432,6 +3446,7 @@ void ice_ptp_release(struct ice_pf *pf)
 	if (pf->ptp.state != ICE_PTP_READY) {
 		mutex_destroy(&pf->ptp.port.ps_lock);
 		ice_ptp_cleanup_pf(pf);
+		ice_ptp_cleanup_adapter(pf);
 		if (pf->ptp.clock) {
 			ptp_clock_unregister(pf->ptp.clock);
 			pf->ptp.clock = NULL;
@@ -3445,6 +3460,8 @@ void ice_ptp_release(struct ice_pf *pf)
 	ice_ptp_disable_timestamp_mode(pf);
 
 	ice_ptp_cleanup_pf(pf);
+
+	ice_ptp_cleanup_adapter(pf);
 
 	ice_ptp_release_tx_tracker(pf, &pf->ptp.port.tx);
 
