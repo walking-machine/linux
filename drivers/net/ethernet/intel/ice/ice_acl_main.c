a596f7bb6161fb8a1ac6c14d3662604cdc00cdbd
Real Valiquette <real.valiquette@intel.com>
2026-07-02 12:30:02 +0200

    ice: create ACL entry
    Create an ACL entry for the mask match data and set the desired action.
    Generate and program the associated extraction sequence.
    
    Co-developed-by: Chinh Cao <chinh.t.cao@intel.com>
    Signed-off-by: Chinh Cao <chinh.t.cao@intel.com>
    Signed-off-by: Real Valiquette <real.valiquette@intel.com>
    Co-developed-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Co-developed-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Co-developed-by: Marcin Szycik <marcin.szycik@linux.intel.com>
    Signed-off-by: Marcin Szycik <marcin.szycik@linux.intel.com>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>

--- drivers/net/ethernet/intel/ice/ice_acl_main.c
+++ drivers/net/ethernet/intel/ice/ice_acl_main.c
@@ -5,6 +5,9 @@
 #include "ice_lib.h"
 #include "ice_acl_main.h"
 
+/* Default ACL Action priority */
+#define ICE_ACL_ACT_PRIO	3
+
 /* Number of action */
 #define ICE_ACL_NUM_ACT		1
 
@@ -208,6 +211,32 @@ free_seg:
 }
 
 /**
+ * ice_acl_set_act_drop - setup drop action
+ * @action: pointer to action
+ */
+static void ice_acl_set_act_drop(struct ice_flow_action *action)
+{
+	action->type = ICE_FLOW_ACT_DROP;
+	action->data.acl_act.mdid = ICE_MDID_RX_PKT_DROP;
+	action->data.acl_act.prio = ICE_ACL_ACT_PRIO;
+	action->data.acl_act.value = cpu_to_le16(ICE_RX_PKT_DROP_DROP);
+}
+
+/**
+ * ice_acl_set_act_fwd_queue - setup forward to queue action
+ * @action: pointer to action
+ * @queue_index: queue index
+ */
+static void ice_acl_set_act_fwd_queue(struct ice_flow_action *action,
+				      s16 queue_index)
+{
+	action->type = ICE_FLOW_ACT_FWD_QUEUE;
+	action->data.acl_act.mdid = ICE_MDID_RX_DST_Q;
+	action->data.acl_act.prio = ICE_ACL_ACT_PRIO;
+	action->data.acl_act.value = cpu_to_le16(queue_index);
+}
+
+/**
  * ice_acl_add_rule_ethtool - add an ACL rule
  * @vsi: pointer to target VSI
  * @cmd: command to add or delete ACL rule
@@ -216,12 +245,57 @@ free_seg:
  */
 int ice_acl_add_rule_ethtool(struct ice_vsi *vsi, struct ethtool_rxnfc *cmd)
 {
+	struct ice_flow_action acts[ICE_ACL_NUM_ACT];
 	struct ethtool_rx_flow_spec *fsp;
+	struct ice_acl_hw_prof *hw_prof;
+	struct ice_ntuple_fltr *input;
+	enum ice_fltr_ptype flow;
+	struct device *dev;
 	struct ice_pf *pf;
+	struct ice_hw *hw;
+	u64 entry_h = 0;
+	int err;
 
 	pf = vsi->back;
+	hw = &pf->hw;
+	dev = ice_pf_to_dev(pf);
 
 	fsp = (struct ethtool_rx_flow_spec *)&cmd->fs;
 
-	return ice_acl_prof_add_ethtool(pf, fsp);
+	err = ice_acl_prof_add_ethtool(pf, fsp);
+	if (err)
+		return err;
+
+	/* Add new rule */
+	input = kzalloc_obj(*input);
+	if (!input)
+		return -ENOMEM;
+
+	err = ice_ntuple_set_input_set(vsi, ICE_BLK_ACL, fsp, input);
+	if (err)
+		goto free_input;
+
+	memset(&acts, 0, sizeof(acts));
+	if (fsp->ring_cookie == RX_CLS_FLOW_DISC)
+		ice_acl_set_act_drop(&acts[0]);
+	else
+		ice_acl_set_act_fwd_queue(&acts[0], input->q_index);
+
+	flow = ice_ethtool_flow_to_fltr(fsp->flow_type & ~FLOW_EXT);
+	hw_prof = hw->acl_prof[flow];
+
+	err = ice_flow_add_entry(hw, ICE_BLK_ACL, hw_prof->prof_id,
+				 fsp->location, vsi->idx, ICE_FLOW_PRIO_NORMAL,
+				 input, acts, ICE_ACL_NUM_ACT, &entry_h);
+	if (err) {
+		dev_err(dev, "Could not add flow entry %d\n", flow);
+		goto free_input;
+	}
+
+	return 0;
+
+free_input:
+	kfree(input);
+
+	return err;
 }
